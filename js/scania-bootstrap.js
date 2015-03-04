de32f776bbf52222376a11daa986b5cd9e9775f8
Daniel Wolmerud <daniel.wolmerud@scania.com>
2015-03-04 12:11:23 +0100

    init commit 1.2.1

--- js/scania-bootstrap.js
+++ js/scania-bootstrap.js
@@ -1,17 +1,3 @@
-$.fn.onReady = function(func) {
-  var elm = this.jquery ? this[0] : this;
-
-  if (document.addEventListener) {
-      elm.addEventListener('DOMContentLoaded', func, false);
-  } else if (window.addEventListener) {
-      elm.addEventListener('load', func, false );
-  } else if (document.attachEvent) {
-      elm.attachEvent("onreadystatechange", func);
-  } else {
-      elm.attachEvent("onload", func);
-  }
-};
-
 // This function adds a script element with chosen src, to the page head section
 var importScript = function(src, id) {
     var head = document.getElementsByTagName("head")[0],
@@ -33,4 +19,4 @@ importScript("https://static.scania.com/development/global/js/bootstrap/scania-b
 // Triggers bootstrapReady when bootstrap framework has been loaded
 $("#bootstrap").on("load", function() {
 	$(document).trigger("bootstrapReady");
-});
+})
